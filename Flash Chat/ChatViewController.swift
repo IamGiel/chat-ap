1df70d3297d5aec20c264fa73daa10904a8e67c2
IamGiel <november14giel@gmail.com>
2019-02-18 15:08:44 -0500

    change database rule

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase;
+import ChameleonFramework
 
 
 class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
@@ -54,6 +55,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
 
         configTableView()
         retrieveMessages()
+        messageTableView.separatorStyle = .none;
     }
 
     ///////////////////////////////////////////
@@ -72,6 +74,14 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.messageBody.text = messageArray[indexPath.row].messageBody
         cell.senderUsername.text = messageArray[indexPath.row].sender
         cell.avatarImageView.image = UIImage(named: "egg")
+        
+        if cell.senderUsername.text == (Auth.auth().currentUser?.email ) {
+            cell.avatarImageView.backgroundColor = UIColor.flatMint()
+            cell.messageBackground.backgroundColor = UIColor.flatSkyBlue()
+        } else {
+            cell.avatarImageView.backgroundColor = UIColor.flatWatermelon() 
+            cell.messageBackground.backgroundColor = UIColor.flatGray()
+        }
         return cell;
     }
     
